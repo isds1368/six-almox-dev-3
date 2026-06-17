f54558dd8e6d08ba4b2df84a494c98ad5eb6d259
isds1368 <soares.isaias2@outlook.com>
2026-06-17 16:44:29 -0300

    Update unidades.py

--- utils/unidades.py
+++ utils/unidades.py
@@ -5,6 +5,7 @@ UNIDADES_MAP = {
     "GL":"GL (Galão)","DZ":"DZ (Dúzia)","CT":"CT (Centena)","SC":"SC (Saco)",
     "FD":"FD (Fardo)","BL":"BL (Bloco)","PT":"PT (Pacote)","PR":"PR (Par)",
     "TB":"TB (Tubo)","CJ":"CJ (Conjunto)","FL":"FL (Folha)","GR":"GR (Grama)",
+    "PLT":"PLT (Palete)",
 }
 SIGLAS = list(UNIDADES_MAP.keys())
 OPCOES = list(UNIDADES_MAP.values())
